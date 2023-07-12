d28621d0506b9838672c22b0d3de574a9c66843d
Sourabhsd87 <102124662+Sourabhsd87@users.noreply.github.com>
2023-07-13 00:45:21 +0530

    Add files via upload

---
+++ Bubble_Sort/src/BubbleSort/Algorithm.java
@@ -0,0 +1,17 @@
+package BubbleSort;
+
+public class Algorithm {
+
+	public static void bubbleSort(int [] arr) {
+		int n=arr.length;
+		for(int i=0;i<n;i++) {
+			for(int j=1;j<n-i;j++) {
+				if(arr[j-1]>arr[j]) {
+					int temp=arr[j-1];
+					arr[j-1]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+	}
+}
---
+++ Bubble_Sort/src/BubbleSort/Tester.java
@@ -0,0 +1,15 @@
+package BubbleSort;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr= {54,25,48,53,76};
+		Algorithm.bubbleSort(arr);
+		for(int i:arr) {
+			System.out.print(i+",");
+		}
+	}
+
+}
+//25,48,53,54,76,

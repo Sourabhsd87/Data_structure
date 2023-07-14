66392503d340bc73aabbf84140a6ecf2e0275826
Sourabhsd87 <102124662+Sourabhsd87@users.noreply.github.com>
2023-07-15 00:50:53 +0530

    Update Tester.java

--- Bubble_Sort/src/BubbleSort/Tester.java
+++ Bubble_Sort/src/BubbleSort/Tester.java
@@ -6,6 +6,7 @@ public class Tester {
 		// TODO Auto-generated method stub
 		int [] arr= {54,25,48,53,76};
 		Algorithm.bubbleSort(arr);
+		System.out.print("Heap sort......")
 		for(int i:arr) {
 			System.out.print(i+",");
 		}
